8c2b20714e6d16e84adeab020ee0ff9862fd38db
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-11-22 22:58:40 +0530

    change the backend url

--- frontend/src/helpers/axiosInstance.js
+++ frontend/src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://website-content-management-system.onrender.com/api/v1";
+const BASE_URL = "https://website-content-management-system.onrender.com/api/v1/";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
