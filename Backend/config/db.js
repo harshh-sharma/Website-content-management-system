07f9c9b45183b64ea00cc1501275e3d8fe76e519
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-11-22 21:12:50 +0530

    some changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 const connectToDb = async () => {
     try {
-        const connection = await mongoose.connect("mongodb://127.0.0.1:27017/content-management-system");
-        if(connection){
-            console.log(`DB connected successfully`);
-        }
+        const connection = await mongoose.connect(process.env.dbConnectionString);
+        console.log(`DB connected successfully to ${connection.connection.name}`);
     } catch (error) {
         console.log(error);
         process.exit(1);
